0ad2c1fba207db0480c5f2f8a7fbf44ff86e9608
diegorodsouza <diegorodsouza@hotmail.com>
2017-04-05 00:54:21 -0300

    +alimentacaoitempage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
-  rootPage: any = TabsPage;
+  rootPage: any = AlimentacaoItemPage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
