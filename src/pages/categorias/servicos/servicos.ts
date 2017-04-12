e582131f561f95ecaec9a4d8d5352a014b30093f
William Lacerda <williamslacerda@gmail.com>
2017-04-12 17:55:03 -0300

    #resolvendo bug do navPush

--- src/pages/categorias/servicos/servicos.ts
+++ src/pages/categorias/servicos/servicos.ts
@@ -19,10 +19,10 @@ export class ServicosPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.itensLista = [
-      {titulo:'Xerox e Gráficas', imagem:'assets/images/servicos/xerox.jpg', page: 'xeroxgraficaPage'},
-      {titulo:'Bancos', imagem:'assets/images/servicos/banco.jpg', page:'bancoPage'},
-      {titulo:'Comércio', imagem:'assets/images/servicos/comercio.jpg', page:'comercioPage'},
-      {titulo:'Outros', imagem:'assets/images/servicos/outros.jpg', page:'outrosPage'},
+      {titulo:'Xerox e Gráficas', imagem:'assets/images/servicos/xerox.jpg', page: this.xeroxgraficaPage},
+      {titulo:'Bancos', imagem:'assets/images/servicos/banco.jpg', page: ''},
+      {titulo:'Comércio', imagem:'assets/images/servicos/comercio.jpg', page: ''},
+      {titulo:'Outros', imagem:'assets/images/servicos/outros.jpg', page: ''},
 
     ];
   }
