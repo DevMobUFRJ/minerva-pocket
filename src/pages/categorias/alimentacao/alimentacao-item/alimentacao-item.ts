c7fc9a1b05caeaf36988a00b72a568fe61d0c62e
diegorodsouza <diegorodsouza@hotmail.com>
2017-10-18 17:56:19 -0200

    +toggle accordion list no alimentacao

--- src/pages/categorias/alimentacao/alimentacao-item/alimentacao-item.ts
+++ src/pages/categorias/alimentacao/alimentacao-item/alimentacao-item.ts
@@ -8,18 +8,33 @@ import { NavController, NavParams } from 'ionic-angular';
 export class AlimentacaoItemPage {
 
   item:any;
+  shownGroup = null;
+  detalhes: Array<{ titulo: string, detalhe: any }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
         this.item = navParams.get('item');
+        this.detalhes = [
+          { titulo: "Tipo de Comida", detalhe: this.item[0].tipo_de_comida },
+          { titulo: "Preços", detalhe: this.item[0].preco },
+          { titulo: "Pagamento", detalhe: this.item[0].tipo_de_pagamento },
+          { titulo: "Funcionamento", detalhe: this.item[0].funcionamento },
+        ]
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AlimentacaoItemPage');
   }
 
-  toggleDetails(data) {
-
-  }
+  toggleGroup(group) {
+    if (this.isGroupShown(group)) {
+      this.shownGroup = null;
+    } else {
+      this.shownGroup = group;
+    }
+  };
+  isGroupShown(group) {
+    return this.shownGroup === group;
+  };
 
   toNumber( LocArray:any ){
     return parseFloat(LocArray);
