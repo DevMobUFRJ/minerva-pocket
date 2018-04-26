97e5dcee4f4ffc2989e5a1c95f692af7ec8a7c72
Karine Cardozo <karinecardozo@ejcm.com.br>
2018-04-25 23:57:13 -0300

    +enviar email de sugestão

--- src/pages/sugestao/sugestao.ts
+++ src/pages/sugestao/sugestao.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 @Component({
     selector: 'page-sugestao',
     templateUrl: 'sugestao.html'
 })
 export class SugestaoPage {
-    
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    nome='';
+    from='';
+    mensagem='';
+    constructor(public navCtrl: NavController, public navParams: NavParams, public emailComposer: EmailComposer) {
 
     }
     ionViewDidLoad() {
         console.log('ionViewDidLoad SugestaoPage');
+    }
+    send(){
+        console.log("cheguei no send");
+        let email = {
+            to: 'karine.cardozo@gmail.com',
+            cc:[],
+            bcc:[],
+            attachment:[],
+            subject: 'Sugestão - Minerva Pocket',
+            body: this.mensagem,
+            isHtml: false
+        }
+        this.emailComposer.open(email);
     }
 }
